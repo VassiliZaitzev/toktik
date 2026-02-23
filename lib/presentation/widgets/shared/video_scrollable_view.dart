6f5256f5b89102535bc472bb5ddca04dd54bcdd1
James Alfred Johnson Montano <jjohnson@MJOLC3MD>
2026-02-23 16:55:16 -0300

    asd

---
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_button.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  final List<VideoPost> videos;
+  
+  const VideoScrollableView({
+    super.key,
+    required this.videos
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      physics: BouncingScrollPhysics(),
+      scrollDirection: Axis.vertical,
+      itemCount: videos.length,
+      itemBuilder: (context, index)  {
+        final VideoPost videoPost = videos[index];
+
+        return Stack(
+          children: [
+            //VIDEO PLAYER + GRADIENTE
+
+            // BOTONES
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost),
+
+            )
+          ],
+        );
+      },
+    );
+  }
+}
